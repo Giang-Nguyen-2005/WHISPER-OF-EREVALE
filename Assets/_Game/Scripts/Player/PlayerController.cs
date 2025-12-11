f13f7db9117574132ac37067ccfaeda1cf9258c8
Giang-Nguyen-2005 <nguyengiang2005a@gmail.com>
2025-12-11 16:46:35 +0700

    feat: add Sub-State Machine (Normal State and Spear State) and its logic

--- Assets/_Game/Scripts/Player/PlayerController.cs
+++ Assets/_Game/Scripts/Player/PlayerController.cs
@@ -1,17 +1,18 @@
 using UnityEngine;
 using System.Collections;
+using Unity.VisualScripting;
 public class PlayerController : MonoBehaviour
 {   
     public float runSpeed = 2.2f;
     public float walkSpeed=1.2f;
 
     public float dashSpeed=5.2f;
-
     public float timeDash=0.18f;
     [SerializeField] private float currentSpeed;
-
     [SerializeField] Vector2 lastDirection;
     public float direction;
+
+    public int weaponType;
     public bool isRunning=false;
 
     public bool isDashing=false;
@@ -20,8 +21,8 @@ public class PlayerController : MonoBehaviour
     private Rigidbody2D rb;
 
     private Vector2 moveInput;
-    private Animator animator;
-    public GameObject shadow;
+    [SerializeField] private Animator playerAnimator;
+    [SerializeField] private Animator shadowAnimator;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -29,8 +30,10 @@ public class PlayerController : MonoBehaviour
 
         rb = GetComponent<Rigidbody2D>();
 
-        animator = GetComponentInChildren<Animator>();
-        shadow=GameObject.Find("Visuals/Shadow");
+        playerAnimator = GameObject.Find("Visuals").GetComponent<Animator>();
+        shadowAnimator=GameObject.Find("Shadow").GetComponent<Animator>();
+        
+        //playerAnimator = GetComponentInChildren<Animator>();
     }
     
     void HandleInput()
@@ -49,9 +52,23 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Z)&&!isDashing)
         {
             StartCoroutine(Dashing());
-        }
+        } 
 
         currentSpeed = isRunning ? runSpeed : walkSpeed;
+        if(Input.GetKeyDown(KeyCode.Alpha2)|| Input.GetKeyDown(KeyCode.Keypad2))
+        {   
+            weaponType=1;
+            playerAnimator.SetInteger("WeaponType",1);
+        }
+        if(Input.GetKeyDown(KeyCode.Alpha1)|| Input.GetKeyDown(KeyCode.Keypad1))
+        {   
+            weaponType=0;
+            playerAnimator.SetInteger("WeaponType",0);
+        }
+        if (Input.GetMouseButtonDown(0) && weaponType==1)
+        {
+            playerAnimator.SetTrigger("Attack");
+        }
     }
     
     // Update is called once per frame
@@ -63,21 +80,24 @@ public class PlayerController : MonoBehaviour
     }
     void UpdateAnimation()
     {
-        if (animator==null) return;
+        if (playerAnimator==null) return;
         else
         {
             //lam muot
-            animator.SetFloat("Speed", moveInput.magnitude*currentSpeed,0.03f,Time.deltaTime);
+            playerAnimator.SetFloat("Speed", moveInput.magnitude*currentSpeed,0.03f,Time.deltaTime);
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            animator.SetTrigger("Jump");
+            playerAnimator.SetTrigger("Jump");
+            //shadowAnimator.SetTrigger("Jump Shadow");
         }
             //Cập nhật hướng
             if (moveInput.magnitude > 0)//magnitude la do dai cua 1 vecto c^2 = a^2 +b^2
         {
             direction = isRunning ? 1 : 0.5f;// 1 là chạy , 0.5 là walk
-                animator.SetFloat("InputX", moveInput.x*direction);
-                animator.SetFloat("InputY", moveInput.y*direction);
+                playerAnimator.SetFloat("InputX", moveInput.x*direction);
+                playerAnimator.SetFloat("InputY", moveInput.y*direction);
+                shadowAnimator.SetFloat("InputX",moveInput.x);
+                shadowAnimator.SetFloat("InputY",moveInput.y);
         }
 
         }
@@ -96,10 +116,19 @@ public class PlayerController : MonoBehaviour
             isDashing=true;
             
             rb.linearVelocity= moveInput*dashSpeed;
-            animator.SetTrigger("Dash_Shadow");
-            animator.SetTrigger("Dash");
+            playerAnimator.SetTrigger("Dash");
+            shadowAnimator.SetTrigger("Dash Shadow");
             yield return new WaitForSeconds(timeDash);
             isDashing=false;
             
         }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("death"))
+        {
+            //shadowTransform.position += Vector3.up*0.15f;
+            playerAnimator.SetTrigger("Death");
+            shadowAnimator.SetTrigger("Death Shadow");
+        }
+    }
 }
